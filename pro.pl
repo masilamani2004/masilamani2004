7da07f5648e285aa8ce6d1fb72a9c13d2114e0bd
masilamani2004 <144878955+masilamani2004@users.noreply.github.com>
2024-04-18 09:38:02 -0700

    Add files via upload

---
+++ pro.pl
@@ -0,0 +1,13 @@
+$str1="hello world";
+$str2="@Welcome BCA@";
+print($str1,"\n");
+print($str2,"\n");
+print(length($str1),"\n");
+print(length($str2),"\n");
+print(uc($str1),"\n");
+print(lc($str1),"\n");
+print(uc($str2),"\n");
+print(lc($str2),"\n");
+$sub="BCA";
+$ find=index($str2,$sub);
+print("the String $str2 in a position and $sub in a String : $find")
